fd1ef017db1b0404ca8cf96c3cd813edc10fbe18
joshuajamesfarrow <joshuajamesfarrow@gmail.com>
2025-09-08 22:46:31 -0400

    Fix Jest configuration for ES modules and TypeScript
    - Update Jest config to use ts-jest/presets/default-esm for ES module support
    - Add extensionsToTreatAsEsm for .ts and .tsx files
    - Configure ts-jest with useESM: true and proper tsconfig
    - Update tsconfig.test.json to use ESNext modules and allowJs
    - Add Babel support for JavaScript files with babel-jest
    - Install required Babel dependencies for React and ES modules
    - Fix 'Cannot use import statement outside a module' errors
    
    All web tests now pass (4/4 test suites)

--- apps/web/jest.config.cjs
+++ apps/web/jest.config.cjs
@@ -1,16 +1,21 @@
 module.exports = {
   rootDir: __dirname,
-  preset: 'ts-jest',
+  preset: 'ts-jest/presets/default-esm',
   testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  extensionsToTreatAsEsm: ['.ts', '.tsx'],
   moduleNameMapper: {
     '^@gbg/types$': '<rootDir>/../../packages/types/src',
     '^d3$': '<rootDir>/../../node_modules/d3/dist/d3.js',
   },
   transform: {
-    '^.+\\.(ts|tsx)$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.test.json' }],
+    '^.+\\.(ts|tsx)$': ['ts-jest', { 
+      tsconfig: '<rootDir>/tsconfig.test.json',
+      useESM: true
+    }],
+    '^.+\\.(js|jsx)$': 'babel-jest',
   },
   transformIgnorePatterns: [
     'node_modules/(?!(d3|d3-[^/]+|internmap|delaunator|robust-predicates)/)',
-  ],
+  ]
 };
