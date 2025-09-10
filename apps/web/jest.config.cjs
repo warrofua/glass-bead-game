444fdbe3a20047be7fde2744bf7d32ee0f7a06aa
Joshua Farrow <41028474+warrofua@users.noreply.github.com>
2025-09-09 23:05:51 -0400

    fix: include graph utilities extension

--- apps/web/jest.config.cjs
+++ apps/web/jest.config.cjs
@@ -5,7 +5,7 @@ module.exports = {
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
   extensionsToTreatAsEsm: ['.ts', '.tsx'],
   moduleNameMapper: {
-    '^@gbg/types$': '<rootDir>/../../packages/types/src',
+    '^@gbg/types$': '<rootDir>/../../packages/types/dist/index.js',
     '^d3$': '<rootDir>/../../node_modules/d3/dist/d3.js',
     '^./App$': '<rootDir>/src/App.tsx',
   },
